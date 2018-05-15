c36d20b0359a98ffa49f8ec941c625563823c2e8
heboyuan <boyuan_heboyuan@live.com>
2018-05-14 23:23:48 -0700

    test

--- project2b/test.sh
+++ project2b/test.sh
@@ -26,6 +26,18 @@ do
 	done
 done
 
+# Run your program with --yield=id, 4 lists, 1,4,8,12,16 threads, and 1, 2, 4, 8, 16 iteratio
+# ns (and no synchronization) to see how many iterations it takes to reliably fail (and make 
+# sure your Makefile expects some of these tests to fail).
+
+for i in 1 4 8 12 16
+do
+	for j in 1 2 4 8 16
+	do
+		./lab2_list --threads=$i  --iterations=$j   --yield=id --lists=4 >> lab2b_list.csv
+	done
+done
+
 # Run your program with --yield=id, 4 lists, 1,4,8,12,16 threads, and 10, 20, 40, 80 iterations
 # , --sync=s and --sync=m to confirm that updates are now properly protected (i.e., all runs su
 # cceeded).
@@ -41,18 +53,5 @@ do
 	for j in 10 20 40 80
 	do
 		./lab2_list --threads=$i  --iterations=$j   --yield=id --lists=4 --sync=m >> lab2b_list.csv
-	done
-done
-
-
-# Run your program with --yield=id, 4 lists, 1,4,8,12,16 threads, and 1, 2, 4, 8, 16 iteratio
-# ns (and no synchronization) to see how many iterations it takes to reliably fail (and make 
-# sure your Makefile expects some of these tests to fail).
-
-for i in 1 4 8 12 16
-do
-	for j in 1 2 4 8 16
-	do
-		./lab2_list --threads=$i  --iterations=$j   --yield=id --lists=4 >> lab2b_list.csv
 	done
 done
