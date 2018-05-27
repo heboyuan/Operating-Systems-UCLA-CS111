4cd9730704fe73a7aac71fa439b0c225ff169b64
heboyuan <boyuan_heboyuan@live.com>
2018-05-27 12:54:52 -0700

    initial fix unfinished

--- project3a/lab3a.c
+++ project3a/lab3a.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv){
   if(argc < 2){
-    fprintf(stderr, "Error: Invalid arguement\n", );
+    fprintf(stderr, "Error: Invalid arguement\n");
     exit(1);
   }
   int fd = open(argv[1], O_RDONLY);
@@ -32,69 +32,87 @@ int main(int argc, char **argv){
   superblock.s_inodes_count, block_size, superblock.s_inode_size, superblock.s_blocks_per_group,
   superblock.s_inodes_per_group, superblock.s_first_ino);
 
+  //====================================================
+  // for multiple group
+  //--------------------------------------------------
+  // int num_group = superblock.s_blocks_count / superblock.s_blocks_per_group;
+  // int group_id;
+  // for(group_id = 0; group_id < num_group; group_id++){
+  //====================================================
+
   //====================================================//
-  //part2 group + part3 free block + part4 free inode   //
+  //part2 group                                         //
   //====================================================//
-  int num_group = superblock.s_blocks_count / superblock.s_blocks_per_group;
-  int group_id;
-  for(group_id = 0; group_id < num_group; group_id++){
-    //====================================================//
-    //part2 group                                         //
-    //====================================================//
-    struct ext2_group_desc cur_group;
-    //potentially problematic !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    pread(fd, &cur_group, sizeof(cur_group), 2048 + group_id*32);
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  struct ext2_group_desc cur_group;
 
-    int blocks_in_group = superblock.s_blocks_per_group;
-    int inodes_in_group = superblock.s_inodes_per_group;
-    if(group_id = num_group - 1){
-      blocks_in_group = superblock.s_blocks_count%superblock.s_blocks_per_group;
-      inodes_in_group = superblock.s_inodes_count%superblock.s_inodes_per_group;
-    }
+  //====================================================
+  // for multiple group
+  //--------------------------------------------------
+  // pread(fd, &cur_group, sizeof(cur_group), 2048 + group_id*32);
+  //====================================================
+  pread(fd, &cur_group, sizeof(cur_group), 2048 );
 
-    fprintf(stdout, "GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", group_id, blocks_in_group,
-    inodes_in_group, cur_group.bg_free_blocks_count, cur_group.bg_free_inodes_count,
-    cur_group.bg_block_bitmap, cur_group.bg_inode_bitmap, cur_group.bg_inode_table)
+  //====================================================
+  // for multiple group
+  //--------------------------------------------------
+  // int blocks_in_group = superblock.s_blocks_per_group;
+  // int inodes_in_group = superblock.s_inodes_per_group;
+  // if(group_id = num_group - 1){
+  //   blocks_in_group = superblock.s_blocks_count%superblock.s_blocks_per_group;
+  //   inodes_in_group = superblock.s_inodes_count%superblock.s_inodes_per_group;
+  // }
+  //
+  //
+  // fprintf(stdout, "GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", group_id, blocks_in_group,
+  // inodes_in_group, cur_group.bg_free_blocks_count, cur_group.bg_free_inodes_count,
+  // cur_group.bg_block_bitmap, cur_group.bg_inode_bitmap, cur_group.bg_inode_table)
+  //====================================================
 
-    //====================================================//
-    //part3 free block                                    //
-    //====================================================//
-    int byte;
-    for(byte = 0; byte < block_size; byte ++){
-      int buffer;
-      pread(fd, &buffer, 1, cur_group.bg_block_bitmap*block_size + i)
-      int bit;
-      for(bit = 0; bit < 8; bit++){
-        if((buffer & (1 << bit)) == 0){
-          fprintf(stdout, "BFREE,%d\n", byte*8+bit+1);
-        }
+  int blocks_in_group = superblock.s_blocks_count%superblock.s_blocks_per_group;
+  int inodes_in_group = superblock.s_inodes_count%superblock.s_inodes_per_group;
+
+  fprintf(stdout, "GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", 0, blocks_in_group,
+  inodes_in_group, cur_group.bg_free_blocks_count, cur_group.bg_free_inodes_count,
+  cur_group.bg_block_bitmap, cur_group.bg_inode_bitmap, cur_group.bg_inode_table);
+
+
+  //====================================================//
+  //part3 free block                                    //
+  //====================================================//
+  unsigned int byte;
+  for(byte = 0; byte < block_size; byte ++){
+    int buffer;
+    pread(fd, &buffer, 1, cur_group.bg_block_bitmap*block_size + byte);
+    unsigned int bit;
+    for(bit = 0; bit < 8; bit++){
+      if((buffer & (1 << bit)) == 0){
+        fprintf(stdout, "BFREE,%d\n", byte*8+bit+1);
       }
     }
-
-    //====================================================//
-    //part4 free inode                                    //
-    //====================================================//
-    int byte;
-    for(byte = 0; byte < block_size; byte ++){
-      int buffer;
-      pread(fd, &buffer, 1, cur_group.bg_inode_bitmap*block_size + i)
-      int bit;
-      for(bit = 0; bit < 8; bit++){
-        if((buffer & (1 << bit)) == 0){
-          fprintf(stdout, "BFREE,%d\n", byte*8+bit+1);
-        }
-      }
-    }
-
-    //====================================================//
-    //part5 inode summary                                 //
-    //====================================================//
-    struct ext2_inode inode;
-    int inode_i;
-    for(inode_i = 0; inode_i < )
   }
 
+  //====================================================//
+  //part4 free inode                                    //
+  //====================================================//
+  for(byte = 0; byte < block_size; byte ++){
+    int buffer;
+    pread(fd, &buffer, 1, cur_group.bg_inode_bitmap*block_size + byte);
+    unsigned int bit;
+    for(bit = 0; bit < 8; bit++){
+      if((buffer & (1 << bit)) == 0){
+        fprintf(stdout, "BFREE,%d\n", byte*8+bit+1);
+      }
+    }
+  }
+
+  //====================================================//
+  //part5 inode summary                                 //
+  //====================================================//
 
 
+  //====================================================
+  // for multiple group
+  //--------------------------------------------------
+  // }
+  //===================================================
 }
