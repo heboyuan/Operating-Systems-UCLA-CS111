5c221061ec956f7b5c4136200e21828f57b7d52d
Liuyi Shi <liuyi.shi@outlook.com>
2018-06-02 17:38:56 -0700

    Reformat code

--- project3b/lab3b.py
+++ project3b/lab3b.py
@@ -35,18 +35,18 @@ class DirectoryEntry:
 
 
 def main():
-    #================================================
-    #Initialization
-    #================================================
+    # ================================================
+    # Initialization
+    # ================================================
     my_bfree = []
     my_ifree = []
     my_inode = []
     my_dir = []
     my_indir = []
 
-    #================================================
-    #Parsing Arguements
-    #================================================
+    # ================================================
+    # Parsing Arguements
+    # ================================================
     if len(sys.argv) != 2:
         sys.stderr.write("Error: Unrecognized arguement")
         sys.exit(1)
@@ -62,14 +62,21 @@ def main():
         elif line_list[0] == "IFREE":
             my_ifree.append(int(line_list[1]))
         elif line_list[0] == "INODE":
-            my_inode.append(inode(int(line_list[1]),line_list[2],int(line_list[3]),int(line_list[4]),int(line_list[5]),int(line_list[6]),line_list[7],line_list[8],line_list[9],int(line_list[10]),int(line_list[11]),list(map(int,line_list[12:]))))
+            my_inode.append(
+                inode(int(line_list[1]), line_list[2], int(line_list[3]), int(line_list[4]), int(line_list[5]),
+                      int(line_list[6]), line_list[7], line_list[8], line_list[9], int(line_list[10]),
+                      int(line_list[11]), list(map(int, line_list[12:]))))
         elif line_list[0] == "DIRENT":
-        	my_dir.append(dir(int(line_list[1]),int(line_list[2]),int(line_list[3]),int(line_list[4]),int(line_list[5]),line_list[6]))
+            my_dir.append(
+                dir(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]), int(line_list[5]),
+                    line_list[6]))
         elif line_list[0] == "INDIRECT":
-        	my_indir.append(indir(int(line_list[1]),int(line_list[2]),int(line_list[3]),int(line_list[4]),int(line_list[5])))
+            my_indir.append(
+                indir(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]), int(line_list[5])))
         else:
-        	sys.stderr.write("Error: Worry data content\n")
-        	sys.exit(1)
+            sys.stderr.write("Error: Worry data content\n")
+            sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
