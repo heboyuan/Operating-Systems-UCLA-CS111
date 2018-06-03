1ab8a0cd42abbd9f18ddeb38f964a516f3bb6de4
Liuyi Shi <liuyi.shi@outlook.com>
2018-06-02 18:18:40 -0700

    indentation fixed

--- project3b/lab3b.py
+++ project3b/lab3b.py
@@ -13,6 +13,9 @@ class SuperBlock:
         self.first_nonreserved_inode = finode
 
 
+class Group:
+    def __init__(self, gnumber):
+
 class Inode:
     def __init__(self, inumber, ftype, mode, owner, group, lcount, ctime, mtime, atime, fsize, dspace, baddress):
         self.inode_number = inumber
@@ -72,23 +75,25 @@ def main():
             my_sp = SuperBlock(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]),
                                int(line_list[5]), int(line_list[6]), int(line_list[7]))
         elif line_list[0] == "GROUP":
-        	my_group = Group(int(line_list[1]),int(line_list[2]),int(line_list[3]),int(line_list[4]),int(line_list[5]),int(line_list[6]),int(line_list[7]),int(line_list[8]))
+            my_group = Group(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]),
+                             int(line_list[5]), int(line_list[6]), int(line_list[7]), int(line_list[8]))
         elif line_list[0] == "BFREE":
             my_bfree.append(int(line_list[1]))
         elif line_list[0] == "IFREE":
             my_ifree.append(int(line_list[1]))
         elif line_list[0] == "INODE":
             my_inode.append(
-                inode(int(line_list[1]), line_list[2], int(line_list[3]), int(line_list[4]), int(line_list[5]),
+                Inode(int(line_list[1]), line_list[2], int(line_list[3]), int(line_list[4]), int(line_list[5]),
                       int(line_list[6]), line_list[7], line_list[8], line_list[9], int(line_list[10]),
                       int(line_list[11]), list(map(int, line_list[12:]))))
         elif line_list[0] == "DIRENT":
             my_dir.append(
-                dir(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]), int(line_list[5]),
-                    line_list[6]))
+                DirectoryEntry(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]),
+                               int(line_list[5]),
+                               line_list[6]))
         elif line_list[0] == "INDIRECT":
             my_indir.append(
-                indir(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]), int(line_list[5])))
+                Indirect(int(line_list[1]), int(line_list[2]), int(line_list[3]), int(line_list[4]), int(line_list[5])))
         else:
             sys.stderr.write("Error: Worry data content\n")
             sys.exit(1)
